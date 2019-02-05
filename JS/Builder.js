7cfa23061809d80dfc87151002ee7d7ec98f3cf0
croz0034 <32398078+croz0034@users.noreply.github.com>
2019-02-05 00:28:51 -0500

    resume

---
+++ JS/Builder.js
@@ -0,0 +1,146 @@
+let Build = {
+    PageHeight : (window.innerHeight - 50),
+    init: (ev)=>{
+        Build.Nav.init(ev);
+        Build.ContactInfo();
+        Build.Work();
+        Build.Education();
+        Build.Projects();
+    },
+    Nav: {
+        init: (ev)=>{
+          let c = Build.Nav;
+            c.NavSizing(ev)
+            c.links()
+        },
+        NavSizing: (ev)=>{
+        let rootElement = document.querySelector(".Navigation");
+        let divide = rootElement.children.length
+            rootElement.querySelectorAll(".NavList").forEach((element)=>{
+            element.style.height = `${(Build.PageHeight)/divide}px`;
+            element.style["padding-top"] = `${(Build.PageHeight)/(divide *2) - 10}px`;
+            element.style["font-size"] = "20px";
+        })
+        },
+        links: ()=>{ document.querySelector("#Navigation").addEventListener("click", Build.Nav.to);
+        document.querySelector('#Mail').addEventListener("click", Build.Mail);
+        document.querySelectorAll(".NavList").forEach((link)=>{
+            link.addEventListener("click", Build.Nav.to)
+        })
+        },
+        to: (ev)=>{
+            ev.target.classList.add("clicked");
+            document.querySelectorAll(".page").forEach((page)=>{page.classList.add("hidden")})
+           document.querySelector("#BannerTitle").textContent = ev.target.id; document.querySelector(`.${ev.target.id}`).classList.remove("hidden")
+        },
+        minimize: (ev)=>{
+            let target = ev.target;
+            if(target.classList.contains("Minimizer")){
+                target = target.parentElement;}
+            if(!target.classList.contains("min")){
+            target.classList.add("min");
+            target.querySelector(".Minimizer").textContent = "+";
+            } else {
+            target.classList.remove("min");
+            target.querySelector(".Minimizer").textContent = "-";
+            }
+        }
+    },
+    ContactInfo: ()=>{
+        let stage = document.querySelector(".Contact");
+        
+        stage.childNodes.forEach((child)=>{
+            if(child.classList){
+                let innerData = resumeContents[child.classList.value]
+                if(Array.isArray(innerData)){
+                    let additions;
+                    innerData.forEach((item)=>{
+                        child.innerHTML += `<ul> ${item} </ul>`
+                    })
+                }
+                else {
+                    child.textContent += innerData;
+                }
+                
+            }
+        })
+    },
+    Work: ()=>{
+        let stage = document.querySelector(".Work");
+        let categories = Object.keys(
+        resumeContents.Work)
+        let additions;
+        categories.forEach((JobType)=>{
+            additions = document.createElement("h4");
+            additions.textContent = JobType;
+            let newStuff = document.createElement("div")
+            newStuff.innerHTML = 
+            ` <p><strong> Location: </strong> ${resumeContents.Work[JobType].Location} </p>
+            <p> <strong>Work Duration:</strong> ${resumeContents.Work[JobType].time} </p>
+            <p> <strong>Description: </strong> <br /> ${resumeContents.Work[JobType].description} </p> 
+<p> <strong>Supervisor:</strong> ${resumeContents.Work[JobType].refference.name} <p>
+<p> <strong>Phone #:</strong> ${resumeContents.Work[JobType].refference.number} <p>`;
+            stage.appendChild(additions);
+            stage.appendChild(newStuff)
+        })
+    },
+    Education: ()=>{
+        let stage = document.querySelector(".Education");
+        Object.keys(resumeContents.Education).forEach((school)=>{
+            stage.innerHTML += `<p><strong>${resumeContents.Education[school].Location}:  <br /></strong>${resumeContents.Education[school].Program} <br />
+             Graduated:${resumeContents.Education[school].Graduated} </p> <br />`
+        })
+        stage.innerHTML += `<p><strong > Current Courses </strong> </p>`;
+        let sections = Object.keys(resumeContents.Marks)
+        let additions = document.createElement("table");
+        stage.appendChild(additions)
+        sections.forEach((semester)=>{
+            additions.innerHTML += `<tr><td></td><td><h4 style="Width: 100%; text-align: center;"> ${semester} </h4></td><td></td></tr>`
+            resumeContents.Marks[semester].forEach((course)=>{
+            let NewElementCourse = document.createElement("tr");
+                NewElementCourse.innerHTML = `
+<td>${course.Code}</td>
+<td>${course.Name}</td>
+<td>${course["Letter Grade"]}</td>`;
+                additions.appendChild(NewElementCourse);
+            })
+        })
+    },
+    Projects: ()=>{
+        document.querySelectorAll(".Project").forEach((zone)=>{zone.addEventListener("click", Build.Nav.minimize)})
+        Object.keys(resumeContents.Projects).forEach((projectType)=>{
+            
+        })
+      
+        let zone = document.querySelector(".School");
+        Build.ProjectFiller(zone, resumeContents.Projects.School)
+        zone = document.querySelector(".Extracurricular");
+        Build.ProjectFiller(zone, resumeContents.Projects["Extracurricular_Learning"])
+        zone = document.querySelector(".Hoby");
+        Build.ProjectFiller(zone, resumeContents.Projects.Hoby)
+        
+},
+    ProjectFiller: (zone, Data)=>{
+        let contentArea = zone.nextElementSibling;
+        let additions;
+        Data.forEach((Project)=>{
+            additions = document.createElement('div');
+            additions.classList.add("Card");
+            additions.innerHTML = `
+<h4 style="text-align: center;"> ${Project.Name} </h4>
+<p> Code: <br /> <a href="${Project.URL}">${Project.URL} </a> </p>
+<p> Working Site: <br /> <a href="${Project.Pages}">${Project.Pages} </a> </p>`
+            if(Array.isArray(Project.Description)){
+            Project.Description.forEach((point)=>{additions.innerHTML += `<li> ${point} </li>`});} else {
+                additions.innerHTML += `<li> ${Project.Description} </li>`
+            }
+            contentArea.appendChild(additions);
+        })
+    },
+    Mail: (ev)=>{
+        console.log('ping')
+        window.location.href = "mailto:tylercrozman@gmail.com?subject=Resume&body=";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", Build.init)
---
+++ JS/Resume.js
@@ -0,0 +1,243 @@
+let resumeContents = {
+        Name: ["Tyler Nathan Crozman"],
+        Email: "tylercrozman@gmail.com",
+        Phone: "(613) 599-6131",
+        Cell: "613-581-6825",
+        Education: {
+            Highschool: {
+                Location: "A.Y. Jackson",
+                Program: "Highschool",
+                Graduated: "2009"
+            },
+            University: {
+                Location: "Carleton University",
+                Program: "Honours in Philosophy, minor in Psycology",
+                Graduated: "2012"
+            },
+            College: {
+                Location: "Algonquin",
+                Program: "Mobile application design and development",
+                Graduated: "(in progress) 2019"
+            },
+        },
+        Marks: {
+            "Level One:": [
+                {
+                    Code: "MAD 8010",
+                    Name: "Digital Citizenship",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9010",
+                    Name: "Graphics Technologies",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9013",
+                    Name: "Cross-Platform Web Development",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9014",
+                    Name: "Cross-Platform Application Development I",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9111",
+                    Name: "Fundamentals of Computer Hardware and Networking",
+                    "Letter Grade": "A+"
+                }],
+            "Level Two:": [
+                {
+                    Code: "DSN 2100M",
+                    Name: "Design Thinking",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "ENL 1813T",
+                    Name: "Communications 1",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9020",
+                    Name: "User Interface Design",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9021",
+                    Name: "Introduction to Object Oriented Programming",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9022",
+                    Name: "Cross-Platform Application Development II",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9023",
+                    Name: "Server-Side Technology for Mobile",
+                    "Letter Grade": "A+"
+                },
+        ],
+            "Level three:": [
+                {
+                    Code: "ENL 8720",
+                    Name: "Technical Report Writing",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9034",
+                    Name: "User Experience Design",
+                    "Letter Grade": "A"
+                },
+                {
+                    Code: "MAD 9132",
+                    Name: "Android Application Development",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9135",
+                    Name: "Developing HTML5  Mobile Applications",
+                    "Letter Grade": "A+"
+                },
+                {
+                    Code: "MAD 9137",
+                    Name: "iOS Development",
+                    "Letter Grade": "A+"
+                },
+        ],
+            "Level Four:":[
+                
+                {
+                    Code: "MAD 9042",
+                    Name: "Best Practices for Mobile Development",
+                    "Letter Grade": "In progress"
+                },
+                {
+                    Code: "MAD 9143",
+                    Name: "Business for the Mobile Industry",
+                    "Letter Grade": "In progress"
+                },
+                {
+                    Code: "MAD 9145",
+                    Name: "Applied Project",
+                    "Letter Grade": "In progress"
+                },
+                {
+                    Code: "MAD 9146",
+                    Name: "Windows Development for Mobile",
+                    "Letter Grade": "In progress"
+                },
+                {
+                    Code: "GEN 1957",
+                    Name: "Science Fiction",
+                    "Letter Grade": "In progress"
+                },
+            ]
+            
+    },
+    Strengths: ["Quick learner", "Hard worker", "My code grows cleaner by the day"],
+    Weaknesses: ["Too ambitious with pet projects.", "Focus is mainly on functionality, and not aesthetics."],
+    Work: {
+        "Part time": {
+            Location: "The Works (Kanata)",
+            time: "2009 - present",
+            description: "Dish washer, and general cleaner. I was a bartender too, but that role was merged with hosting.",
+            refference: {
+                name: "",
+                number: ""
+            }
+        },
+        "Freelance": {
+            Location: "Home",
+            time: "Christmas break 2018",
+            description: "I was called in by a classmate to help with some node JS code. It processed POST requests, translated them into JSON, converted that into HTML and PDF formats, which were then e-mailed to a preset destination.",
+            refference: {
+                name: "Shawn Boxiao Liu",
+                number: "(613) 794-6866"
+            }
+        }
+    },
+    Projects: {
+        School: [
+            {
+                Name: "Movie Reccomendations",
+                URL: "https://github.com/croz0034/Movie-Recommendations",
+                Pages: "https://croz0034.github.io/Movie-Recommendations/",
+                Description: "We were learning to use online API and make fetch calls."
+        },
+            {
+                Name: "The Singularity",
+                URL: "https://github.com/croz0034/ReactiveFire",
+                Pages: "https://croz0034.github.io/ReactiveFire/",
+                Description: ["A javascript list/description project with similar to 'project pheonix.' ", " It was made to be put inside a cordova wrapper, and uploaded onto an android device."]
+        },
+            {
+                Name: "Firebase with React",
+                URL: "https://github.com/croz0034/CordovaFinal",
+                Pages: "https://croz0034.github.io/CordovaFinal/",
+                Description: ["The project goal was to make a project using the 'react' framework, that also incorporated google's 'firebase' for data storage", "We were also assigned a 'react native' project at the same time. I used this one to test features for that one, which is why the interface does not impress", "I managed to add touch controlls without refferencing the DOM (because the DOM does not exist in react native)", " And took it one step further, using key inputs rather than the values from the text input fields to build the input strings"]
+        }
+        ],
+        "Extracurricular_Learning": [
+            {
+                Name: "Function Shortcuts",
+                URL: "https://github.com/croz0034/FunctionShortcut",
+                Pages: "https://croz0034.github.io/FunctionShortcut/",
+                Description: [
+                "Started as my attempt to learn how to use touch controlls in javascript, it snowballed from there as I went to learn more.",
+                "Many of the features are implemented on elements during the initialization function, by searching through the document for class name keywords, meaning no further javascript pointers should be necessary.",
+                "I also started toying with the idea of colour pickers, list generators (from JSON objects), and other snippets of code that would be useful for an HTML builder GUI"
+                ]
+        },
+            {
+                Name: "Box Draw",
+                URL: "https://github.com/croz0034/FunctionShortcut",
+                Pages: "https://croz0034.github.io/FunctionShortcut/PlacementTest",
+                Description: [
+                "This sub-page was my forrays into making a GUI for building HTML",
+                "It fell by the wayside as school began again, and I ran into issues including all the HTML attribute and style options in an easy to use manner"
+                ]
+        },
+            {
+                Name: "Block Push",
+                URL: "https://github.com/croz0034/BlockPush",
+                Pages: "https://croz0034.github.io/BlockPush/",
+                Description: [
+                "I wanted to see if I could make a javascript generated game with what I knew.",
+                "It includes swipe controlls for touch screens and WASD movement for keyboards",
+                "Levels are stored in the cloud on firebase, for ease of acess on multiple devices",
+                "Levels themselves are relatively compact JSON objects (the main map being an array of arrays of 3character strings)",
+                "Terrain effects are extrapolated through those 3 character strings, by targetting corresponding methods through their string literals in the code.",
+                "This is an active WIP"
+                ]
+        }
+        ],
+        Hoby: [
+            {
+                Name: "Project Pheonix",
+                URL: "https://github.com/croz0034/Project-Pheonix",
+                Pages: "https://croz0034.github.io/Project-Pheonix/",
+                Description: ["An early take on an interractive refference book for the live action roleplaying game 'Amtgard.' ", " This version used bootstrap for formatting ", " I eventually scrapped the project as I didn't like my formatting"]
+        },
+            {
+                Name: "Summer Singularity",
+                URL: "https://github.com/croz0034/SummerSingularity",
+                Pages: "https://croz0034.github.io/SummerSingularity/",
+                Description: [" A far more ambitious take on 'the singularity' school project.", "It has an optional login function to track experience", "It was built with mobile browsing in mind, so it does not look good on desktop.", "The project was a bit too ambitious (including a spell list builder with math, save functions, and data manipulation) for how much interest the LARP group was showing in it. So debugging and updates fell by the wayside."]
+        },
+            {
+                Name: "Tournament Companion",
+                URL: "https://github.com/croz0034/TryeltechTourneyCompanion",
+                Pages: "https://croz0034.github.io/TryeltechTourneyCompanion/",
+                Description: [
+                "I made an organizer for the 'Amtgard' tournament scene.",
+            "Using code from a friend, I was able to acess the online database, and grab a registry of all the players and their achievements",
+            "Currently there are 3 working tournament styles with 3 more on the way",
+            "Tournament brackets are generated with code, and win streaks are kept track of for award purposes",
+            "Modular options allow for multiple different brackets",
+            "Double elimination has been a consistent headache due to it's late introduction."]
+        }
+        ]
+    }
+}
